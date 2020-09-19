a0c1b98514b196adf39b768d8108859ee2a1e91f
Gileade Trindade <gileade.trindade@gmail.com>
2020-09-19 18:09:19 -0300

    Inserindo o ModelMapper no projeto, e refatorando LivroController

--- src/main/java/br/com/gile/livrariaapi/api/resource/LivroController.java
+++ src/main/java/br/com/gile/livrariaapi/api/resource/LivroController.java
@@ -3,6 +3,7 @@ package br.com.gile.livrariaapi.api.resource;
 import br.com.gile.livrariaapi.api.dto.LivroDTO;
 import br.com.gile.livrariaapi.api.model.entity.Livro;
 import br.com.gile.livrariaapi.service.LivroService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,28 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class LivroController {
 
     private LivroService service;
+    private ModelMapper modelMapper;
 
-    public LivroController(LivroService service) {
+    public LivroController(LivroService service, ModelMapper modelMapper) {
         this.service = service;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public LivroDTO cria(@RequestBody LivroDTO dto){
-        Livro entidade =
-                Livro.builder()
-                    .autor(dto.getAutor())
-                    .titulo(dto.getTitulo())
-                    .isbn(dto.getIsbn())
-                    .build();
-
+        Livro entidade = modelMapper.map(dto, Livro.class);
         entidade = service.save(entidade);
-
-        return LivroDTO.builder()
-                .id(entidade.getId())
-                .autor(entidade.getAutor())
-                .titulo(entidade.getTitulo())
-                .isbn(entidade.getIsbn())
-                .build();
+        return modelMapper.map(entidade, LivroDTO.class);
     }
 }
