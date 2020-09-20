dbba2a07d58fe70c56f861ff95f008e45c07853c
Gileade Trindade <gileade.trindade@gmail.com>
2020-09-20 13:07:45 -0300

    Implementando o Teste criaLivroComIsbnDuplicado

--- src/main/java/br/com/gile/livrariaapi/api/exception/ApiErrors.java
+++ src/main/java/br/com/gile/livrariaapi/api/exception/ApiErrors.java
@@ -1,8 +1,10 @@
 package br.com.gile.livrariaapi.api.exception;
 
+import br.com.gile.livrariaapi.exception.BusinessException;
 import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ApiErrors {
@@ -12,6 +14,11 @@ public class ApiErrors {
         this.errors = new ArrayList<>();
         bindingResult.getAllErrors().forEach(error -> this.errors.add(error.getDefaultMessage()));
     }
+
+    public ApiErrors(BusinessException ex){
+        this.errors = Arrays.asList(ex.getMessage());
+    }
+
 
     public List<String> getErrors() {
         return errors;
--- src/test/java/br/com/gile/livrariaapi/api/resource/LivroControllerTest.java
+++ src/test/java/br/com/gile/livrariaapi/api/resource/LivroControllerTest.java
@@ -2,6 +2,7 @@ package br.com.gile.livrariaapi.api.resource;
 
 import br.com.gile.livrariaapi.api.dto.LivroDTO;
 import br.com.gile.livrariaapi.api.model.entity.Livro;
+import br.com.gile.livrariaapi.exception.BusinessException;
 import br.com.gile.livrariaapi.service.LivroService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -46,7 +47,7 @@ public class LivroControllerTest {
     @DisplayName("Deve criar um livro com sucesso.")
     public void criaLivroTest() throws Exception{
         //Cria um Objeto Livro DTO
-        LivroDTO dto = LivroDTO.builder().autor("Gile").titulo("God of War").isbn("001").build();
+        LivroDTO dto = criaNovoLivro();
         //Cria um livro Mockado
         Livro livroSalvo = Livro.builder().id(10l).autor("Gile").titulo("God of War").isbn("001").build();
 
@@ -80,6 +81,7 @@ public class LivroControllerTest {
         ;
     }
 
+
     @Test
     @DisplayName("Deve lançar erro de validação quando não houver dados suficiente para criação do livro.")
     public void criaLivroInvalidoTest() throws Exception {
@@ -95,5 +97,31 @@ public class LivroControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("errors", hasSize(3)))
         ;
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro ao tentar cadastrar um livro com isbn já cadastrado anteriormente.")
+    public void criaLivroComIsbnDuplicado() throws Exception{
+        LivroDTO dto = criaNovoLivro();
+        String json = new ObjectMapper().writeValueAsString(dto);
+        String mensagemErro = "Isbn já cadastrado.";
+
+        BDDMockito.given(service.save(Mockito.any(Livro.class))).willThrow(new BusinessException(mensagemErro));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(LIVRO_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0]").value(mensagemErro));
+        ;
+    }
+
+    private LivroDTO criaNovoLivro() {
+        return LivroDTO.builder().autor("Gile").titulo("God of War").isbn("001").build();
     }
 }
