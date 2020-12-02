56e6bfb4242fd13ecb65c5f752462ba7aa28bac4
Gileade Trindade <gileade.trindade@gmail.com>
2020-12-02 16:23:23 -0300

    Criando Email Service.

--- src/main/java/br/com/gile/livrariaapi/LivrariaApiApplication.java
+++ src/main/java/br/com/gile/livrariaapi/LivrariaApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,11 +13,6 @@ public class LivrariaApiApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
-	}
-
-	@Scheduled(cron = "0 37 15 1/1 * ?")//http://www.cronmaker.com/
-	public void teste(){
-		System.out.println("Executou a parada");
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/gile/livrariaapi/service/AgendamentoService.java
@@ -0,0 +1,33 @@
+package br.com.gile.livrariaapi.service;
+
+import br.com.gile.livrariaapi.model.entity.Emprestimo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AgendamentoService {
+
+    private static final String CRON_EMPRESTIMOS_ATRASADOS = "0 0 0 1/1 * ?";
+
+    @Value("${application.email.emprestimoatrasado.mensagem}")
+    private String mensagem;
+
+    private final EmprestimoService emprestimoService;
+    private final EmailService emailService;
+
+    @Scheduled(cron = CRON_EMPRESTIMOS_ATRASADOS)
+    public void enviaEmailParaEmprestimosAtrasados(){
+        List<Emprestimo> allEmprestimosAtrasados = emprestimoService.getAllEmprestimosAtrasados();
+        List<String> listaDeEmails = allEmprestimosAtrasados.stream()
+                .map(emprestimo -> emprestimo.getEmailCliente())
+                .collect(Collectors.toList());
+
+        emailService.enviaEmails(listaDeEmails,mensagem);
+    }
+}
