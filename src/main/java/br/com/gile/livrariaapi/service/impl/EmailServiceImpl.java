f975ef78237b7ba472f7cb8f3021798641ce91ba
Gileade Trindade <gileade.trindade@gmail.com>
2020-12-02 16:48:55 -0300

    Implementando o serviço de e-mail.

--- src/main/java/br/com/gile/livrariaapi/service/impl/EmailServiceImpl.java
+++ src/main/java/br/com/gile/livrariaapi/service/impl/EmailServiceImpl.java
@@ -1,12 +1,33 @@
 package br.com.gile.livrariaapi.service.impl;
 
 import br.com.gile.livrariaapi.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${application.email.default-remetente}")
+    private String remetente;
+
+    private final JavaMailSender javaMailSender;
+
     @Override
     public void enviaEmails(List<String> listaDeEmails, String mensagem) {
+        String[] emails = listaDeEmails.toArray(new String[listaDeEmails.size()]);
 
+        SimpleMailMessage mensagemDeEmail = new SimpleMailMessage();
+        mensagemDeEmail.setFrom(remetente);
+        mensagemDeEmail.setSubject("Livro com empréstimo atrasado");
+        mensagemDeEmail.setText(mensagem);
+        mensagemDeEmail.setTo(emails);
+
+        javaMailSender.send(mensagemDeEmail);
     }
 }
