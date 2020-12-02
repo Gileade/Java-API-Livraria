e742b1fd47ee564b260b0f16d33b5b85ba312647
Gileade Trindade <gileade.trindade@gmail.com>
2020-12-02 16:40:34 -0300

    Adicionando propriedades de envio de e-mail.

--- src/main/java/br/com/gile/livrariaapi/service/EmailService.java
+++ src/main/java/br/com/gile/livrariaapi/service/EmailService.java
@@ -2,8 +2,6 @@ package br.com.gile.livrariaapi.service;
 
 import java.util.List;
 
-public class EmailService {
-    public void enviaEmails(List<String> listaDeEmails, String mensagem) {
-
-    }
+public interface EmailService {
+    public void enviaEmails(List<String> listaDeEmails, String mensagem);
 }
---
+++ src/main/java/br/com/gile/livrariaapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package br.com.gile.livrariaapi.service.impl;
+
+import br.com.gile.livrariaapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+    @Override
+    public void enviaEmails(List<String> listaDeEmails, String mensagem) {
+
+    }
+}
